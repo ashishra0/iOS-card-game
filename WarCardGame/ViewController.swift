656c8117124cfa9e59dd065aa6f5d1415929c114
Ashish Rao <ashishrao2598@gmail.com>
2021-02-02 11:29:42 +0530

    finish game logic

--- WarCardGame/ViewController.swift
+++ WarCardGame/ViewController.swift
@@ -14,21 +14,38 @@ class ViewController: UIViewController {
     @IBOutlet weak var rightImageView: UIImageView!
 
     @IBOutlet weak var leftScoreLabel: UILabel!
-    
+
     @IBOutlet weak var rightScoreLabel: UILabel!
-    
+
+    var leftScore: Int = 0
+    var rightScore: Int = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    
-    @IBAction func dealTapped(_ sender: Any) {
-        
-        let leftNumber = Int.random(in: 2...14)
-        let rightNumber = Int.random(in: 2...14)
 
+    @IBAction func dealTapped(_ sender: Any) {
+
+        // Randomize numbers
+        let leftNumber: Int = Int.random(in: 2...14)
+        let rightNumber: Int = Int.random(in: 2...14)
+
+        // update the image views
         leftImageView.image = UIImage(named: "card\(leftNumber)")
         rightImageView.image = UIImage(named: "card\(rightNumber)")
+
+        // Compare the random numbers
+        if leftNumber > rightNumber {
+            leftScore += 1
+            leftScoreLabel.text = String(leftScore)
+        }
+        else if leftNumber < rightNumber {
+            rightScore += 1
+            rightScoreLabel.text = String(rightScore)
+        } else {
+            // Tie
+        }
     }
 }
 
